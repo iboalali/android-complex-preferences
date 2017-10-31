e6d62aedc89466805a8a89887394d7d671ba6c3f
Ibrahim Al-Alali <iboalali@gmail.com>
2017-11-01 00:27:58 +0100

    added a few function from sharedPrefrences
    getall() + SetonSharedPreferenceChangeListener() + apply() + contains

--- ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferences.java
+++ ComplexPreferences/src/br/com/kots/mob/complex/preferences/ComplexPreferences.java
@@ -1,71 +1,90 @@
 package br.com.kots.mob.complex.preferences;
 
 import java.lang.reflect.Type;
+import java.util.Map;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+/**
+ * Created by fsilvestremorais, https://github.com/fsilvestremorais/android-complex-preferences
+ * Modified by iboalali on 03-Oct-17.
+ */
+
 public class ComplexPreferences {
+    private static ComplexPreferences complexPreferences;
+    //private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener;
+    private static Gson GSON = new Gson();
+    Type typeOfObject = new TypeToken<Object>() {
+    }.getType();
 
-	private static ComplexPreferences complexPreferences;
-	private Context context;
-	private SharedPreferences preferences;
-	private SharedPreferences.Editor editor;
-	private static Gson GSON = new Gson();
-	Type typeOfObject = new TypeToken<Object>() {
-	}.getType();
+    private ComplexPreferences(Context context, String namePreferences, int mode) {
+        //this.context = context;
+        if (namePreferences == null || namePreferences.equals("")) {
+            namePreferences = "complex_preferences";
+        }
+        preferences = context.getSharedPreferences(namePreferences, mode);
+        editor = preferences.edit();
+    }
 
-	private ComplexPreferences(Context context, String namePreferences, int mode) {
-		this.context = context;
-		if (namePreferences == null || namePreferences.equals("")) {
-			namePreferences = "complex_preferences";
-		}
-		preferences = context.getSharedPreferences(namePreferences, mode);
-		editor = preferences.edit();
-	}
+    public static ComplexPreferences getComplexPreferences(Context context, String namePreferences, int mode) {
+        if (complexPreferences == null) {
+            complexPreferences = new ComplexPreferences(context, namePreferences, mode);
+        }
 
-	public static ComplexPreferences getComplexPreferences(Context context,
-			String namePreferences, int mode) {
+        return complexPreferences;
+    }
 
-		if (complexPreferences == null) {
-			complexPreferences = new ComplexPreferences(context,
-					namePreferences, mode);
-		}
+    public void registerOnSharedPreferenceChangeListener(SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener){
+        preferences.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+        this.onSharedPreferenceChangeListener = onSharedPreferenceChangeListener;
+    }
 
-		return complexPreferences;
-	}
+    public void putObject(String key, Object object) {
+        if(object == null){
+            throw new IllegalArgumentException("object is null");
+        }
 
-	public void putObject(String key, Object object) {
-		if(object == null){
-			throw new IllegalArgumentException("object is null");
-		}
-		
-		if(key.equals("") || key == null){
-			throw new IllegalArgumentException("key is empty or null");
-		}
-		
-		editor.putString(key, GSON.toJson(object));
-	}
+        if(key.equals("") || key == null){
+            throw new IllegalArgumentException("key is empty or null");
+        }
 
-	public void commit() {
-		editor.commit();
-	}
+        editor.putString(key, GSON.toJson(object));
+    }
 
-	public <T> T getObject(String key, Class<T> a) {
-	
-		String gson = preferences.getString(key, null);
-		if (gson == null) {
-			return null;
-		} else {
-			try{
-				return GSON.fromJson(gson, a);
-			} catch (Exception e) {
-				throw new IllegalArgumentException("Object storaged with key " + key + " is instanceof other class");				
-			}
-		}
-	}
-		
-	
+    public boolean contains(String key){
+        return preferences.contains(key);
+    }
+
+    public Map<String, ?> getAll(){
+        return preferences.getAll();
+    }
+
+    public void commit() {
+        editor.commit();
+    }
+
+    public void apply(){
+        editor.apply();
+    }
+
+    public <T> T getObject(String key, Class<T> a) {
+
+        String gson = preferences.getString(key, null);
+        if (gson == null) {
+            return null;
+        } else {
+            try{
+                return GSON.fromJson(gson, a);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Object storaged with key " + key + " is instanceof other class");
+            }
+        }
+    }
 }
